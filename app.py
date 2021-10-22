0a4fe7dc5b71aba1c4520b6b61c316dc264add52
Mohamed Agamy <mohamedagamy327@gmail.com>
2021-10-22 15:21:38 +0200

    cors

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
+
+from flask_cors import CORS
 from flask import Flask, render_template, request, jsonify
+
 import pandas as pd
+
 
 app = Flask(__name__)
 app.debug = True
+
+CORS(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
-@app.route('/data', methods=['GET', 'POST'])
+@app.route('/api/data', methods=['GET', 'POST'])
 def data():
+
     if request.method == 'POST':
         file = request.files['upload-file']
         data = pd.read_csv(file)
